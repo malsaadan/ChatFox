8ae9121b77317c826515393154618328e879f2a8
malsaadan <mmalsaadan@gmail.com>
2020-02-26 16:05:06 +0300

    Updates

---
+++ precache-manifest.30e9b1a8fd8bd557525bbb86ce82c78b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d09a6ec1fcd45faf97eb568b37a6e0a8",
+    "url": "/ChatFox/index.html"
+  },
+  {
+    "revision": "85455acd003aa74118a6",
+    "url": "/ChatFox/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "845fefd05e9e0c00e9cc",
+    "url": "/ChatFox/static/css/main.a8137d89.chunk.css"
+  },
+  {
+    "revision": "85455acd003aa74118a6",
+    "url": "/ChatFox/static/js/2.e2d04643.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/ChatFox/static/js/2.e2d04643.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "845fefd05e9e0c00e9cc",
+    "url": "/ChatFox/static/js/main.46087e02.chunk.js"
+  },
+  {
+    "revision": "f17f84899eea95276bf8",
+    "url": "/ChatFox/static/js/runtime-main.1df6241f.js"
+  },
+  {
+    "revision": "951a5886cdc33db9da79a86339ee8cb5",
+    "url": "/ChatFox/static/media/forest.951a5886.jpeg"
+  },
+  {
+    "revision": "b4ce467cdd90f7dc9a5232a25f62dfd3",
+    "url": "/ChatFox/static/media/logo2.b4ce467c.png"
+  }
+]);
